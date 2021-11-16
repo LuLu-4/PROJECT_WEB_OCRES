e768f49ad68492d0da26118ed5ebfbf021c4bb47
pierrelapalut <pierre.lapalut@edu.ece.fr>
2021-11-16 17:22:26 +0100

    add more pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Calendrier from './pages/Calendrier';
+import Classements from './pages/Classements';
+import Equipes from './pages/Equipes';
+import Pilotes from './pages/Pilotes';
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/a-propos" exact component={About} />
+          <Route path="/a-calendrier" exact component={Calendrier} />
+          <Route path="/a-classements" exact component={Classements} />
+          <Route path="/a-equipes" exact component={Equipes} />
+          <Route path="/a-pilotes" exact component={Pilotes} />
           <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -10,6 +10,18 @@ const Navigation = () => {
             <NavLink exact to="/a-propos" activeClassName="nav-active">
                 A propos
             </NavLink>
+            <NavLink exact to="/a-calendrier" activeClassName="nav-active">
+                Calendrier
+            </NavLink>
+            <NavLink exact to="/a-classements" activeClassName="nav-active">
+                Classements
+            </NavLink>
+            <NavLink exact to="/a-equipes" activeClassName="nav-active">
+                Equipes
+            </NavLink>
+            <NavLink exact to="/a-pilotes" activeClassName="nav-active">
+                Pilotes
+            </NavLink>
         </div>
     )
 
