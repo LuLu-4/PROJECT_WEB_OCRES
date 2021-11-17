77399a146937c6789aab56681ab1e403a122a06a
pierrelapalut <pierre.lapalut@edu.ece.fr>
2021-11-17 18:09:34 +0100

    Styles Navigation.js

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
+import 'C:/Users/Pierre Lapalut/Documents/PROJECT_WEB_OCRES/frontend/src/styles/navigation.css';
+import logo from 'C:/Users/Pierre Lapalut/Documents/PROJECT_WEB_OCRES/frontend/src/formula-1-logo.png';
 
 const Navigation = () => {
     return (
         <div className="Navigation">
-            <NavLink exact to="/" activeClassName="nav-active">
+            <img src={logo} width="100" height="50" />
+            <NavLink exact to="/" className="nav-active">
                 Acceuil
             </NavLink>
-            <NavLink exact to="/a-propos" activeClassName="nav-active">
+            <NavLink exact to="/a-propos" className="nav-active">
                 A propos
             </NavLink>
-            <NavLink exact to="/a-calendrier" activeClassName="nav-active">
+            <NavLink exact to="/a-calendrier" className="nav-active">
                 Calendrier
             </NavLink>
-            <NavLink exact to="/a-classements" activeClassName="nav-active">
+            <NavLink exact to="/a-classements" className="nav-active">
                 Classements
             </NavLink>
-            <NavLink exact to="/a-equipes" activeClassName="nav-active">
+            <NavLink exact to="/a-equipes" className="nav-active">
                 Equipes
             </NavLink>
-            <NavLink exact to="/a-pilotes" activeClassName="nav-active">
+            <NavLink exact to="/a-pilotes" className="nav-active">
                 Pilotes
             </NavLink>
         </div>
